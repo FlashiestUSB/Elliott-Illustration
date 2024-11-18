5b27eae42cce9896cb77afe5939da9a38888bf1c
FlashiestUSB <rjdelemotte@gmail.com>
2024-11-19 00:17:41 +0200

    initial commit

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template, url_for, redirect, flash
+from dotenv import load_dotenv
+from flask_bootstrap import Bootstrap5
+import os
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+@app.route('/')
+def home():
+
+    return render_template('index.html')
+
+@app.route('/products')
+def products():
+
+    return render_template('products.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
