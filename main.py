6ce074323d427e1fe975a117b9166d9ab05bc23c
FlashiestUSB <rjdelemotte@gmail.com>
2024-11-19 17:39:55 +0200

    kinda ready

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask, request, render_template, url_for, redirect, flash
 from dotenv import load_dotenv
 import os
@@ -5,20 +7,32 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('FLASK_KEY')
+
+
+def get_year():
+    acc_date = datetime.datetime.now()
+    return acc_date.year
 
 
 @app.route('/')
 def home():
 
-    return render_template('index.html')
+    return render_template('index.html', year=get_year())
 
 
 @app.route('/products')
 def products():
 
-    return render_template('products.html')
+    return render_template('products.html', year=get_year())
+
+
+@app.route('/contact')
+def contact_me():
+
+    return render_template('contactme.html', year=get_year())
+
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
